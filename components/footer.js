14b6082c222af91740c8750c2a211510f9197a18
Tom Heather <tom.heather@e2x.co.uk>
2020-01-19 17:18:40 +0000

    ye

--- components/footer.js
+++ components/footer.js
@@ -10,9 +10,9 @@ export default () => {
 							<div className="col-xs-12 col-sm-2">
 								<img src="static/img/logo-white-footer.png" alt="Logo Footer" className="mb-4 footer-logo" />
 								<div className="footer-socials">
-									<a href="https://www.facebook.com/AbbeyBoxOffice" className="social-icon si-small si-light si-facebook" title="Facebook" target="_blank">
-										<i className="icon-facebook"></i>
-										<i className="icon-facebook"></i>
+									<a href="https://www.linkedin.com/company/abbey-leisure-group" className="social-icon si-small si-light si-facebook" title="Facebook" target="_blank">
+										<i className="icon-linkedin"></i>
+										<i className="icon-linkedin"></i>
 									</a>
 									<a href="https://twitter.com/abbeyleisuregrp" className="social-icon si-small si-light si-twitter" title="Twitter" target="_blank">
 										<i className="icon-twitter"></i>
