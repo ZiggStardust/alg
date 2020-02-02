782a5ca9653540f998ef8c00f675d3614e612a19
Tom Heather <tom.heather@e2x.co.uk>
2020-02-02 21:42:17 +0000

    ye

--- pages/_document.js
+++ pages/_document.js
@@ -7,14 +7,14 @@ export default class CustomDocument extends Document {
 		return (
 			<html lang='en-US'>
 			<Head>
-				{/*<title>Home</title>*/}
+				<title>Abbey Leisure Group</title>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+				<meta name="description" content="The best in client entertainment, with the hottest tickets and hospitality packages around."></meta>
 
 				<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=swap" rel="stylesheet" />
 				<link href="http://fonts.googleapis.com/css?family=Lato:300,400,400italic,600,700|Raleway:300,400,500,600,700|Crete+Round:400italic" rel="stylesheet" type="text/css" />
 				<link rel="stylesheet" href="static/css/bootstrap.css" type="text/css" />
-
 				<link rel="stylesheet" href="static/css/style.css" type="text/css" />
 				<link rel="stylesheet" href="static/css/dark.css" type="text/css" />
 				<link rel="stylesheet" href="static/css/dark-rtl.css" type="text/css" />
